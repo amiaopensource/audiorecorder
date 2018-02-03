7859cf2e20c769810ae0a89fc56bd24165bfd9cf
Andrew Weaver <weevz@uw.edu>
2018-02-02 16:31:20 -0800

    start preview

--- audiorecorder2-dev/audiorecorder2/audiorecorder2.rb
+++ audiorecorder2-dev/audiorecorder2/audiorecorder2.rb
@@ -76,10 +76,16 @@ Shoes.app(title: "Welcome to AudioRecorder", width: 600, height: 500) do
 
     def PostRecord(targetfile)
       window(title: "Edit BWF Metadata", width: 600, height: 500) do
+        trimcheck = ''
         para targetfile
         waveform = image("AUDIORECORDERTEMP.png")
         para $ffprobeout['streams'][0]['duration']
         duration = $ffprobeout['streams'][0]['duration']
+        preview = button "Preview"
+        preview.click do
+          command = 'mpv --force-window --no-terminal --keep-open=yes --title="Preview" --geometry=620x620 -lavfi-complex "[aid1]asplit=3[ao][a][b],[a]showwaves=600x240:n=1[a1],[a1]drawbox=0:0:600:240:t=120[a2],[b]showwaves=600x240:mode=cline:colors=0x00FFFF:split_channels=1[b2],[a2][b2]overlay[vo]" ' + targetfile
+          system(command)
+        end
       end
     end
 
