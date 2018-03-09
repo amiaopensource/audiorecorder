67e60d80855aebdf82b61500a35a7a5213be7018
Andrew Weaver <weevz@uw.edu>
2018-03-09 14:32:08 -0800

    move and add buttons

--- audiorecorder2-dev/audiorecorder2/audiorecorder2.rb
+++ audiorecorder2-dev/audiorecorder2/audiorecorder2.rb
@@ -81,8 +81,9 @@ Shoes.app(title: "AudioRecorder2", width: 600, height: 500) do
         trimcheck = nil
         @pretrim = $outputdir + '/' + File.basename(targetfile, File.extname(targetfile)) + '_untrimmed' + '.wav'
         @finaloutput = targetfile
-        para targetfile
-        waveform = image("AUDIORECORDERTEMP.png")
+        stack margin: 15 do
+          waveform = image("AUDIORECORDERTEMP.png")
+        end
         @start_trim = nil
         @end_trim_length = nil
 
@@ -100,18 +101,10 @@ Shoes.app(title: "AudioRecorder2", width: 600, height: 500) do
             $start_trim_opt = ' -ss ' + @start_trim_length.to_s
           end
         end
-
         stack do
-          preview = button "Preview"
-          preview.click do
-            if trimcheck.nil?
-              command = 'mpv --force-window --no-terminal --keep-open=yes --title="Preview" --geometry=620x620 -lavfi-complex "[aid1]asplit=3[ao][a][b],[a]showwaves=600x240:n=1[a1],[a1]drawbox=0:0:600:240:t=120[a2],[b]showwaves=600x240:mode=cline:colors=0x00FFFF:split_channels=1[b2],[a2][b2]overlay[vo]" ' + '"' + @finaloutput + '"'
-              system(command)
-            else
-              command = 'mpv --force-window --no-terminal --keep-open=yes --title="Preview" --geometry=620x620 -lavfi-complex "[aid1]asplit=3[ao][a][b],[a]showwaves=600x240:n=1[a1],[a1]drawbox=0:0:600:240:t=120[a2],[b]showwaves=600x240:mode=cline:colors=0x00FFFF:split_channels=1[b2],[a2][b2]overlay[vo]" ' + '"'+ @pretrim + '"'
-              system(command)
-            end
-          end 
+          para "Press 'Preview' to hear the file you recorded.\nTo trim file, enter the amount (in seconds) to trim from the start and end of the file and press 'Trim'.\nIf 'Start Trim' is left blank, auto-trim will be applied to start of file.  If no trim at start is desired enter '0'. After trimming, a preview window will open for your new file.  Trim can be run as many times as is neccessary.\nPress 'Finish' to quit"
+        end
+        flow do
           @start_trim_length = "AUTO"
           para 'Start Trim'
           start_trim_input = edit_line text = @start_trim_length do
@@ -122,45 +115,61 @@ Shoes.app(title: "AudioRecorder2", width: 600, height: 500) do
             end
           end
         end
-        stack do
+        flow do
           para 'End Trim'
           end_trim_input = edit_line do
             @end_trim_length = end_trim_input.text.to_f
           end
         end
-        trim = button "Trim"
-        trim.click do
-          #set up trim
-          if trimcheck.nil?
-            SetUpTrim(@finaloutput)
-            File.rename(@finaloutput, @pretrim)
-            trimcheck = 1
-          else
-            SetUpTrim(@pretrim)
-          end
-          if @start_trim_length == "AUTO"
-            if ! @end_trim_length.nil? && @end_trim_length != 0.0
-              precommand = 'ffmpeg -i ' + '"' + @pretrim + '"' + ' -af silenceremove=start_threshold=-57dB:start_duration=1:start_periods=1 -f wav -c:a ' + $codec_choice  + ' -ar ' + $sample_rate_choice + ' -y -rf64 auto ' + 'INTERMEDIATE.wav'
-              system(precommand)
-              SetUpTrim('INTERMEDIATE.wav')
-              postcommand = 'ffmpeg -i INTERMEDIATE.wav -c copy -y -rf64 auto ' + ' -t ' + $end_trim_opt.to_s + ' "' + @finaloutput + '"'
-              system(postcommand)
-              File.delete('INTERMEDIATE.wav')
-            else
-              command = 'ffmpeg -i ' + '"' + @pretrim + '"' + ' -af silenceremove=start_threshold=-57dB:start_duration=1:start_periods=1 -f wav -c:a ' + $codec_choice  + ' -ar ' + $sample_rate_choice + ' -y -rf64 auto ' + '"' + @finaloutput + '"'
-              system(command)
-            end
-          else
-            if ! @end_trim_length.nil? && @end_trim_length != 0.0
-              command = 'ffmpeg ' + $start_trim_opt + ' -i ' + '"' + @pretrim + '"' + ' -c copy -y -rf64 auto ' + ' -t ' + $end_trim_opt.to_s + ' "' + @finaloutput + '"'
+        flow do
+          preview = button "Preview"
+          preview.click do
+            if trimcheck.nil?
+              command = 'mpv --force-window --no-terminal --keep-open=yes --title="Preview" --geometry=620x620 -lavfi-complex "[aid1]asplit=3[ao][a][b],[a]showwaves=600x240:n=1[a1],[a1]drawbox=0:0:600:240:t=120[a2],[b]showwaves=600x240:mode=cline:colors=0x00FFFF:split_channels=1[b2],[a2][b2]overlay[vo]" ' + '"' + @finaloutput + '"'
               system(command)
             else
-              command = 'ffmpeg ' + $start_trim_opt + ' -i ' + '"' + @pretrim + '"' + ' -c copy -y -rf64 auto ' + ' "' + @finaloutput + '"'              
+              command = 'mpv --force-window --no-terminal --keep-open=yes --title="Preview" --geometry=620x620 -lavfi-complex "[aid1]asplit=3[ao][a][b],[a]showwaves=600x240:n=1[a1],[a1]drawbox=0:0:600:240:t=120[a2],[b]showwaves=600x240:mode=cline:colors=0x00FFFF:split_channels=1[b2],[a2][b2]overlay[vo]" ' + '"'+ @pretrim + '"'
               system(command)
             end
+          end 
+          trim = button "Trim"
+          trim.click do
+            #set up trim
+            if trimcheck.nil?
+              SetUpTrim(@finaloutput)
+              File.rename(@finaloutput, @pretrim)
+              trimcheck = 1
+            else
+              SetUpTrim(@pretrim)
+            end
+            if @start_trim_length == "AUTO"
+              if ! @end_trim_length.nil? && @end_trim_length != 0.0
+                precommand = 'ffmpeg -i ' + '"' + @pretrim + '"' + ' -af silenceremove=start_threshold=-57dB:start_duration=1:start_periods=1 -f wav -c:a ' + $codec_choice  + ' -ar ' + $sample_rate_choice + ' -y -rf64 auto ' + 'INTERMEDIATE.wav'
+                system(precommand)
+                SetUpTrim('INTERMEDIATE.wav')
+                postcommand = 'ffmpeg -i INTERMEDIATE.wav -c copy -y -rf64 auto ' + ' -t ' + $end_trim_opt.to_s + ' "' + @finaloutput + '"'
+                system(postcommand)
+                File.delete('INTERMEDIATE.wav')
+              else
+                command = 'ffmpeg -i ' + '"' + @pretrim + '"' + ' -af silenceremove=start_threshold=-57dB:start_duration=1:start_periods=1 -f wav -c:a ' + $codec_choice  + ' -ar ' + $sample_rate_choice + ' -y -rf64 auto ' + '"' + @finaloutput + '"'
+                system(command)
+              end
+            else
+              if ! @end_trim_length.nil? && @end_trim_length != 0.0
+                command = 'ffmpeg ' + $start_trim_opt + ' -i ' + '"' + @pretrim + '"' + ' -c copy -y -rf64 auto ' + ' -t ' + $end_trim_opt.to_s + ' "' + @finaloutput + '"'
+                system(command)
+              else
+                command = 'ffmpeg ' + $start_trim_opt + ' -i ' + '"' + @pretrim + '"' + ' -c copy -y -rf64 auto ' + ' "' + @finaloutput + '"'              
+                system(command)
+              end
+            end
+            command = 'mpv --force-window --no-terminal --keep-open=yes --title="Preview" --geometry=620x620 -lavfi-complex "[aid1]asplit=3[ao][a][b],[a]showwaves=600x240:n=1[a1],[a1]drawbox=0:0:600:240:t=120[a2],[b]showwaves=600x240:mode=cline:colors=0x00FFFF:split_channels=1[b2],[a2][b2]overlay[vo]" ' + '"' + @finaloutput + '"'
+            system(command)
           end
-          command = 'mpv --force-window --no-terminal --keep-open=yes --title="Preview" --geometry=620x620 -lavfi-complex "[aid1]asplit=3[ao][a][b],[a]showwaves=600x240:n=1[a1],[a1]drawbox=0:0:600:240:t=120[a2],[b]showwaves=600x240:mode=cline:colors=0x00FFFF:split_channels=1[b2],[a2][b2]overlay[vo]" ' + '"' + @finaloutput + '"'
-          system(command)
+          close = button "Finish"
+          close.click do
+            close()
+          end
         end
       end
     end
