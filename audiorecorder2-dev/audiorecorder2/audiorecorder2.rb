c694e9dd334a1078216c08886b98e4fedca0339e
Andrew Weaver <weevz@uw.edu>
2018-06-28 15:36:57 -0700

    make test for ffmpeg version

--- audiorecorder2-dev/audiorecorder2/audiorecorder2.rb
+++ audiorecorder2-dev/audiorecorder2/audiorecorder2.rb
@@ -22,11 +22,19 @@ else
   Drawfontpath = 'some windows path'
 end
 
+# Check FFmpeg version for filter options
+Ffmpegtest = `ffmpeg -version`.split(' ')[2].to_f
+if Ffmpegtest >= 4
+    Avectorscopefilter = 'avectorscope=s=300x300:r=30:zoom=5:mirror=0'
+else
+    Avectorscopefilter = 'avectorscope=s=300x300:r=30:zoom=5'
+end
+
 FILTER_CHAIN = "asplit=6[out1][a][b][c][d][e],\
 [e]showvolume=w=700:c=0xff0000:r=30[e1],\
 [a]showfreqs=mode=bar:cmode=separate:size=300x300:colors=magenta|yellow[a1],\
 [a1]drawbox=12:0:3:300:white@0.2[a2],[a2]drawbox=66:0:3:300:white@0.2[a3],[a3]drawbox=135:0:3:300:white@0.2[a4],[a4]drawbox=202:0:3:300:white@0.2[a5],[a5]drawbox=271:0:3:300:white@0.2[aa],\
-[b]avectorscope=s=300x300:r=30:zoom=5[b1],\
+[b]#{Avectorscopefilter}[b1],\
 [b1]drawgrid=x=150:y=150:c=white@0.3[bb],\
 [c]showspectrum=s=400x600:slide=scroll:mode=combined:color=rainbow:scale=lin:saturation=4[cc],\
 [d]astats=metadata=1:reset=1,adrawgraph=lavfi.astats.Overall.Peak_level:max=0:min=-30.0:size=700x256:bg=Black[dd],\
